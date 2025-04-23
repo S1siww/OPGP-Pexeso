cc47f06041bcefc82e30c97ca610c1a04de84a8c
martinkozar <kozarmartin86@gmail.com>
2025-04-23 08:09:46 +0200

    funkcia na resetovanie hry

--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
+import random
+
+def nova_hra():
+    karty = list("AABBCCDDEEFFGGHH")
+    random.shuffle(karty)
+    return {
+        "karty": karty,
+        "odhalene": [False] * len(karty),
+        "spojene": [],
+        "otocene": [],
+        "hrac_narade": 1,
+        "skore": {1: 0, 2: 0}
+    }
+
 def zmen_hraca(hrac):
     return 2 if hrac == 1 else 2
