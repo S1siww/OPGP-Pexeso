8e392d0a54c8c80aa51edd928d077f381ac9eb83
SyroXik <gernathjak@gmail.com>
2025-04-30 08:05:19 +0200

    opravena nefunkcnost uvodnej obrazovky

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 import pygame
 from config import WIDTH, HEIGHT
 from game import Game
-
+from graphics import draw_board
+from menu import zobraz_menu
+ 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Pexeso")
-
+ 
+    zobraz_menu(screen)
+ 
     game = Game(screen)
+ 
     running = True
-    koniec = False
-
     while running:
-        if not koniec:
-            game.draw()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    game.handle_click(event.pos)
-                    if game.je_koniec():
-                        koniec = True
-        else:
-            tlacidlo = game.zobraz_vysledok()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    if tlacidlo.collidepoint(event.pos):
-                        game = Game(screen)
-                        koniec = False
-
+        game.draw()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                game.handle_click(event.pos)
+ 
     pygame.quit()
-
+ 
 if __name__ == "__main__":
-    main()
+     main()
