95f32b434ec3a4448559122756da21ff27799d36
SyroXik <gernathjak@gmail.com>
2025-05-14 08:19:29 +0200

    posunutie textu dole

--- gameover.py
+++ gameover.py
@@ -12,13 +12,13 @@ def zobraz_gameover(screen, skore):
     screen.blit(background_image, (bg_x, bg_y))
 
     vysledok_text = config.FONT.render("Koniec hry!", True, config.CIERNA)
-    screen.blit(vysledok_text, (config.WIDTH // 2 - vysledok_text.get_width() // 2, 80))
+    screen.blit(vysledok_text, (config.WIDTH // 2 - vysledok_text.get_width() // 2, 180))
 
     skore1 = config.MALY_FONT.render(f"Hráč 1: {skore[1]} bodov", True, config.MODRA)
     skore2 = config.MALY_FONT.render(f"Hráč 2: {skore[2]} bodov", True, config.CERVENA)
 
-    screen.blit(skore1, (config.WIDTH // 2 - skore1.get_width() // 2, 150))
-    screen.blit(skore2, (config.WIDTH // 2 - skore2.get_width() // 2, 190))
+    screen.blit(skore1, (config.WIDTH // 2 - skore1.get_width() // 2, 250))
+    screen.blit(skore2, (config.WIDTH // 2 - skore2.get_width() // 2, 290))
 
     if skore[1] > skore[2]:
         vitaz_text = config.MALY_FONT.render("Vyhral hráč 1!", True, config.MODRA)
@@ -27,15 +27,15 @@ def zobraz_gameover(screen, skore):
     else:
         vitaz_text = config.MALY_FONT.render("Remíza!", True, config.CIERNA)
 
-    screen.blit(vitaz_text, (config.WIDTH // 2 - vitaz_text.get_width() // 2, 240))
+    screen.blit(vitaz_text, (config.WIDTH // 2 - vitaz_text.get_width() // 2, 340))
 
-    spat_rect = pygame.Rect(config.WIDTH // 2 - 100, 320, 200, 60)
+    spat_rect = pygame.Rect(config.WIDTH // 2 - 100, 420, 200, 60)
     pygame.draw.rect(screen, config.SIVA, spat_rect)
     text = config.MALY_FONT.render("Späť", True, config.CIERNA)
     screen.blit(text, (spat_rect.centerx - text.get_width() // 2,
                        spat_rect.centery - text.get_height() // 2))
 
-    nova_hra_rect = pygame.Rect(config.WIDTH // 2 - 100, 400, 200, 60)
+    nova_hra_rect = pygame.Rect(config.WIDTH // 2 - 100, 500, 200, 60)
     pygame.draw.rect(screen, config.SIVA, nova_hra_rect)
     nova_hra_text = config.MALY_FONT.render("Nová hra", True, config.CIERNA)
     screen.blit(nova_hra_text, (nova_hra_rect.centerx - nova_hra_text.get_width() // 2,
